801a885e0327ddf05a271bd30de4edc6ee271088
orholam <josiaha@uchicago.edu>
2024-12-10 08:16:01 -0600

    small analytics change

--- frontend/src/pages/AnalyticsPage.tsx
+++ frontend/src/pages/AnalyticsPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Analytics } from "tenable-analytics";
 
-const AnalyticsPage = () => (
-  <div>
+const AnalyticsPage = ({ isDarkMode }: { isDarkMode: boolean }) => (
+  <div className={isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}>
     <h1>Site Analytics</h1>
     <Analytics className="w-96 h-96 border-4 border-red-500 m-10" />
   </div>
